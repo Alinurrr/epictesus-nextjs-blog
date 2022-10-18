0dcb0d7411998e988de6332d14adf04aeb3d536b
Alinurrr <alinoer.rachman@gmail.com>
2022-10-18 19:24:13 +0700

    featured post

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import FeaturedPost from './component/FeaturedPost'
 import Navbar from './component/Navbar'
 
 export default function Home() {
   return (
     <div className='bg-gradient-to-b from-gray-600 to-gray-900 min-h-screen text-white'>
       <Navbar />
+      <div className="container mx-auto">
+        <FeaturedPost />
+      </div>
     </div>
   )
 }
