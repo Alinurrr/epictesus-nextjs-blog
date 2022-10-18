189f1a82e0f7a750b40f690389abf91860437163
Alinurrr <alinoer.rachman@gmail.com>
2022-10-18 20:15:30 +0700

    make info post to be component

--- pages/component/FeaturedPost.js
+++ pages/component/FeaturedPost.js
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
+import InfoPost from './InfoPost'
 
 export default function FeaturedPost() {
   return (
@@ -13,26 +14,16 @@ export default function FeaturedPost() {
           />
         </div>
         <div className="w-4/12 px-4">
-          <div className="flex items-center text-white/60 space-x-4">
-            <div className="uppercase">
-              UI Design
-            </div>
-            <span>&bull;</span>
-            <div>Okt 18, 2022</div>
-          </div>
-          <h2 className="text-2xl mt-4">Understanding color theory: the color wheel and finding complementary colors</h2>
-          <p className="text-white/60 mt-6 w-10/12">
-            Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum.
-          </p>
-          <div className="flex items-center mt-6">
-            <Image src="/author-1.png" alt="author photo" className='rounded-full object-cover' width={56} height={56} />
-            <div className="ml-4">
-              <h3>Leslie Alexander</h3>
-              <div className="text-white/60 text-sm">
-                UI Designer
-              </div>
-            </div>
-          </div>
+          <InfoPost
+            category="UI DESIGN"
+            date="Okt 18, 2020"
+            title="Understanding color theory: the color wheel and finding complementary colors"
+            shortDescription="Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum."
+            authorAvatar="/author-1.png"
+            authorName="Leslie Alexander"
+            authorJob="UI Designer"
+
+          />
         </div>
       </div>
     </article >
---
+++ pages/component/InfoPost.js
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function InfoPost({
+  category,
+  date,
+  title,
+  shortDescription,
+  authorAvatar,
+  authorName,
+  authorJob
+}) {
+  return (
+    <>
+      <div className="flex items-center text-white/60 space-x-4">
+        <div className="uppercase">
+          {category}
+        </div>
+        <span>&bull;</span>
+        <div>{date}</div>
+      </div>
+      <h2 className="text-2xl mt-4">{title}</h2>
+      <p className="text-white/60 mt-6 w-10/12">
+        {shortDescription}
+      </p>
+      <div className="flex items-center mt-6">
+        <Image src={authorAvatar} alt="author photo" className='rounded-full object-cover' width={56} height={56} />
+        <div className="ml-4">
+          <h3>{authorName}</h3>
+          <div className="text-white/60 text-sm mt-1">
+            U{authorJob}
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
