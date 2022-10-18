1315ffb3b7a4f0d09b1f466298b8744372593dda
Alinurrr <alinoer.rachman@gmail.com>
2022-10-19 01:15:54 +0700

    make content for empty state posts

--- pages/posts.js
+++ pages/posts.js
@@ -7,21 +7,28 @@ import SectionHeader from './component/SectionHeader';
 
 
 export default function posts() {
-  const [posts, setPosts] = useState(mockPost);
+  const [posts, setPosts] = useState([mockPost]);
   return (
     <>
       <Layout>
-        <SectionHeader>
-          UI Design
-        </SectionHeader>
         <Container>
-          <div className="flex flex-wrap -mx-8 mt-4">
-            {posts.map((post, index) => (
-              <div key={index} className="md:w-4/12 w-full px-4 py-6">
-                <CardPost {...post} />
+          <SectionHeader>UI Design</SectionHeader>
+          {
+            !posts.length ? (
+              <div className="text-center py-20">
+                <h2 className='text-6xl'>No result 😥</h2>
+                <p className='text-xl mt-4 text-white/60 md:w-6/12 w-full mx-auto'>We couldn't find any posts with the keyword `yahahahahahyuk`. Please try another keyword.</p>
               </div>
-            ))}
-          </div>
+            ) : (
+              <div className="flex flex-wrap -mx-8 mt-4">
+                {posts.map((post, index) => (
+                  <div key={index} className="md:w-4/12 w-full px-4 py-6">
+                    <CardPost {...post} />
+                  </div>
+                ))}
+              </div>
+            )
+          }
         </Container>
       </Layout>
     </>
