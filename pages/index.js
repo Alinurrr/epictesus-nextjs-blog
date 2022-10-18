99d480c6273809e796fa8fc0964c82e678ba4b6f
Alinurrr <alinoer.rachman@gmail.com>
2022-10-18 18:34:34 +0700

    navbar component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './component/Navbar'
 
 export default function Home() {
   return (
-    <div className='flex justify-center items-center h-screen'>
-      <h1 className='text-2xl'>Hello Next Js</h1>
+    <div className='bg-gradient-to-b from-gray-600 to-gray-900 min-h-screen text-white'>
+      <Navbar />
     </div>
   )
 }
