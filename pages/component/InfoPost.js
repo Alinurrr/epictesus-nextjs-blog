e7d95311dfbf8a1585d74867f20224318b6c9674
Alinurrr <alinoer.rachman@gmail.com>
2022-10-19 13:51:49 +0700

    make component for post and create detail page

--- pages/component/InfoPost.js
+++ pages/component/InfoPost.js
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
+import PostMetaTitle from './PostMetaTitle'
+import PostAuthor from './PostAuthor'
 
 export default function InfoPost({
   category,
@@ -12,26 +14,19 @@ export default function InfoPost({
 }) {
   return (
     <>
-      <div className="flex items-center text-white/60 space-x-4">
-        <div className="uppercase">
-          {category}
-        </div>
-        <span>&bull;</span>
-        <div>{date}</div>
-      </div>
-      <h2 className="text-2xl mt-4">{title}</h2>
+      <PostMetaTitle
+        category={category}
+        date={date}
+        title={title}
+      />
       <p className="text-white/60 mt-5 w-10/12">
         {shortDescription}
       </p>
-      <div className="flex items-center mt-5">
-        <Image src={authorAvatar} alt="author photo" className='rounded-full object-cover' width={56} height={56} />
-        <div className="ml-4">
-          <h3>{authorName}</h3>
-          <div className="text-white/60 text-sm mt-1">
-            {authorJob}
-          </div>
-        </div>
-      </div>
+      <PostAuthor
+        authorAvatar={authorAvatar}
+        authorJob={authorJob}
+        authorName={authorName}
+      />
     </>
   )
 }
---
+++ pages/detail.js
@@ -0,0 +1,48 @@
+import Image from 'next/image';
+import React from 'react';
+import Container from './component/Container';
+import Layout from './component/Layout';
+import PostAuthor from './component/PostAuthor';
+import PostMetaTitle from './component/PostMetaTitle';
+
+export default function detail() {
+  return (
+    <Layout>
+      <Container>
+        <div className="md:w-6/12 w-full mx-auto flex flex-col items-center">
+          <PostMetaTitle
+            category="UI Desing"
+            date="July 2, 2021"
+            title="Understanding color theory: the color wheel and finding complementary colors"
+            center
+          />
+          <PostAuthor
+            authorName="Leslie Alexander"
+            authorJob="UI Designer"
+            authorAvatar="/author-1.png"
+          />
+
+        </div>
+        <div className="md:w-10/12 w-full sm:h-96 h-60 relative mx-auto my-10">
+          <Image
+            src="/detail-image.png" alt="featured thumbnail" className='rounded-xl '
+            layout="fill"
+            objectFit="cover"
+          />
+        </div>
+        <div className="md:w-8/12 w-full mx-auto leading-relaxed">
+          <p className="text-xl mb-4">
+            Male sixth sea it a. Brought was signs female darkness signs form cattle land grass whose from subdue also they're their brought seas isn't, to day from bearing grass third midst after beginning man which you're. Dry, gathering beginning given made them evening.
+          </p>
+          <p className="mb-4">
+            Lights dry. Thing, likeness, forth shall replenish upon abundantly our green. Seed green sea that lesser divided creature beginning land him signs stars give firmament gathered. Wherein there their morning a he grass. Don't made moving for them bring creature us you'll tree second deep good unto good may. Us yielding.
+          </p>
+          <p>
+            Have. Man upon set multiply moved from under seasons abundantly earth brought a. They're open moved years saw isn't morning darkness. Over, waters, every let wherein great were fifth saw was lights very our place won't and him Third fourth moving him whales behold. Beast second stars lights great was don't green give subdue his. Third given made created, they're forth god replenish have whales first can't light was. Herb you'll them beast kind divided. Were beginning fly air multiply god Yielding sea don't were forth.
+          </p>
+        </div>
+
+      </Container>
+    </Layout>
+  )
+}
