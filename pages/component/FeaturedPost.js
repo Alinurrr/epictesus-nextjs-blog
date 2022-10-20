f83f532923e808dfcb94d0d627810868e23e819c
Alinurrr <alinoer.rachman@gmail.com>
2022-10-20 20:46:44 +0700

    skeleton loading image wiht onLoadingComplete

--- pages/component/FeaturedPost.js
+++ pages/component/FeaturedPost.js
@@ -4,34 +4,34 @@ import React, { useEffect, useState } from 'react'
 import InfoPost from './InfoPost'
 
 export default function FeaturedPost() {
-  const [thumbnail, setThumbnail] = useState(null)
+  const [thumbnailLoading, setThumbnailLoading] = useState(true)
 
-  useEffect(() => {
-    setThumbnail('/public/featured-thumbnail.png')
-    // console.log(thumbnail)
-  }, [])
+  // useEffect(() => {
+  //   setThumbnail('/public/featured-thumbnail.png')
+  //   // console.log(thumbnail)
+  // }, [])
 
   return (
     <article>
       <div className="flex flex-wrap -mx-4 items-center">
 
         {
-          thumbnail ?
-            <div className="px-4 lg:w-8/12 md:w-7/12 w-full sm:h-96 h-60 relative mb-4 md:mb-0 transition-all">
-              <Link href="/detail">
-                <a>
-                  <Image
-                    src="/featured-thumbnail.png" alt="featured thumbnail" className='rounded-xl '
-                    layout="fill"
-                    objectFit="cover"
-                    loading="lazy"
-                  />
-                </a>
-              </Link>
-            </div>
-            :
-            <div className="px-4 lg:w-8/12 md:w-7/12 w-full sm:h-96 h-60 relative mb-4 md:mb-0 animate-pulse bg-slate-200 rounded-xl" />
+          thumbnailLoading &&
+          <div className="px-4 lg:w-8/12 md:w-7/12 w-full sm:h-96 h-60 relative mb-4 md:mb-0 animate-pulse bg-slate-200 rounded-xl" />
         }
+        <div className="px-4 lg:w-8/12 md:w-7/12 w-full sm:h-96 h-60 relative mb-4 md:mb-0 transition-all">
+          <Link href="/detail">
+            <a>
+              <Image
+                src="/featured-thumbnail.png" alt="featured thumbnail" className='rounded-xl '
+                layout="fill"
+                objectFit="cover"
+                loading="lazy"
+                onLoadingComplete={() => setThumbnailLoading(false)}
+              />
+            </a>
+          </Link>
+        </div>
 
         <div className="lg:w-4/12 md:w-5/12 w-full px-4">
           <InfoPost
