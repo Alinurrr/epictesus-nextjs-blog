d67885b0afd8dcaaad86dfba44ac7f1eb300b541
Alinurrr <alinoer.rachman@gmail.com>
2022-10-20 21:16:55 +0700

    thumbnail post image skeleton loading

--- pages/component/CardPost.js
+++ pages/component/CardPost.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import InfoPost from './InfoPost'
 
 export default function CardPost({ thumbnail, ...xxxx }) {
+  const [thumbnailLoading, setThumbnailLoading] = useState(true)
+
   return (
     <article>
       <div className="w-full h-52 relative mb-4">
@@ -13,7 +15,12 @@ export default function CardPost({ thumbnail, ...xxxx }) {
               src={thumbnail} alt="featured thumbnail" className='rounded-xl '
               layout="fill"
               objectFit="cover"
+              onLoadingComplete={() => setThumbnailLoading(false)}
             />
+            {
+              thumbnailLoading &&
+              <div className="w-full h-full animate-pulse bg-slate-200 rounded-xl absolute" />
+            }
           </a>
         </Link>
       </div>
