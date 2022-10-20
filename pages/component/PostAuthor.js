0389691e51950b76b00ed68c8f7c64193d3ba3d5
Alinurrr <alinoer.rachman@gmail.com>
2022-10-20 21:13:34 +0700

    image profile skeleton loading

--- pages/component/PostAuthor.js
+++ pages/component/PostAuthor.js
@@ -1,13 +1,23 @@
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function PostAuthor({
   authorAvatar, authorName, authorJob
 }) {
+
+  const [loadingImage, setLoadingImage] = useState(true)
+
   return (
     <>
       <div className="flex items-center mt-5">
-        <Image src={authorAvatar} alt="author photo" className='rounded-full object-cover' width={56} height={56} />
+        <Image
+          src={authorAvatar} alt="author photo"
+          className='rounded-full object-cover'
+          width={56} height={56}
+          onLoadingComplete={() => setLoadingImage(false)}
+        />
+        {loadingImage && <div className='rounded-full w-14 h-14 animate-pulse bg-slate-200 absolute' />}
+
         <div className="ml-4">
           <h3>{authorName}</h3>
           <div className="text-white/60 text-sm mt-1">
